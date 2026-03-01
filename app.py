d5ab127ef1584cd4999c18b36e405b5d8c5dd416
yousuf873 <168662942+yousuf873@users.noreply.github.com>
2026-03-01 16:19:26 +0500

    Update input method and prediction messages
    Refactor input handling for transaction features and update prediction messages.

--- app.py
+++ app.py
@@ -3,24 +3,29 @@ import streamlit as st
 import joblib
 import numpy as np
 
+# Load Model
 model = joblib.load("aml_lr_model.pkl")
 scaler = joblib.load("aml_scaler.pkl")
 
 st.title("Financial Crime Detection System")
 
-amount = st.number_input("Transaction Amount")
-old_balance = st.number_input("Old Balance")
-new_balance = st.number_input("New Balance")
+st.write("Enter Transaction Features")
+
+inputs = []
+
+for i in range(12):
+    val = st.number_input(f"Feature {i+1}")
+    inputs.append(val)
 
 if st.button("Predict Fraud"):
 
-    input_data = np.array([[amount, old_balance, new_balance]])
+    input_array = np.array([inputs])
 
-    input_scaled = scaler.transform(input_data)
+    input_scaled = scaler.transform(input_array)
 
     prediction = model.predict(input_scaled)
 
     if prediction[0] > 0.5:
-        st.error("⚠ Fraud Detected")
+        st.error(" Fraud Transaction Detected")
     else:
-        st.success("✅ Normal Transaction")
+        st.success(" Normal Transaction")
