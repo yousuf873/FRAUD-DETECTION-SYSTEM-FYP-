c3a148516fce2d8fe5a6613fd4fa3587eb25ccf3
yousuf873 <168662942+yousuf873@users.noreply.github.com>
2026-03-01 17:29:30 +0500

    Update title and feature input prompts in app.py

--- app.py
+++ app.py
@@ -5,26 +5,19 @@ import numpy as np
 model = joblib.load("aml_lr_model.pkl")
 scaler = joblib.load("aml_scaler.pkl")
 
-st.title("Financial Crime Detection System")
-
-st.write("Enter Transaction Features")
-
-# ⭐ Must match training feature size = 5194
+st.title("Financial Crime Detection")
 
 inputs = []
 
-for i in range(5194):
-    inputs.append(st.number_input(f"F{i+1}", value=0.0))
+for i in range(12):
+    inputs.append(st.number_input(f"Feature {i+1}", value=0.0))
 
-if st.button("Predict Fraud"):
-
+if st.button("Predict"):
     input_array = np.array(inputs).reshape(1, -1)
-
     input_scaled = scaler.transform(input_array)
-
     prediction = model.predict(input_scaled)
 
-    if prediction[0] > 0.5:
-        st.error("⚠ Fraud Detected")
+    if prediction[0] == 1:
+        st.error("⚠ Laundering Detected")
     else:
         st.success("✅ Normal Transaction")
